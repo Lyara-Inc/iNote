b5956d75dba41b72d0bb523d89631e7d86e50934
ClemLya <clementlyara72@gmail.com>
2021-06-22 19:09:21 +0200

    V0.0.1

--- app/src/main/java/com/app/inote/MainActivity.kt
+++ app/src/main/java/com/app/inote/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Vairiable de type nullable
+        var name : String? = "Bob"
+
+        val age = 4
+        val height = 1.60F
+
+        println("""Raw string trim :
+        >Nom : $name
+        >Age : $age
+        """.trimMargin(">"))
+
+
+
     }
 }
